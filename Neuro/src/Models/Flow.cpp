577c1cc9c53923aa920005a3710a590bf585e160
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-09-26 12:20:58 -0400

    Reenabled flow model cloning.

--- Neuro/src/Models/Flow.cpp
+++ Neuro/src/Models/Flow.cpp
@@ -192,45 +192,36 @@ namespace Neuro
 
         auto& sourceFlow = static_cast<const Flow&>(source);
 
+        m_OutputsShapes = sourceFlow.m_OutputsShapes;
+
 		// clone is not a frequently used functionality so I'm not too concerned about its performance
 
 		// make clones first and store then in dictionary
-		//map<string, LayerBase*> clones;
+		map<string, LayerBase*> clones;
+		for (auto layer : sourceFlow.m_Order)
+		{
+			auto clone = layer->Clone();
+			clones[clone->Name()] = clone;
+		}
 
-		//for (auto layer : sourceFlow.m_Order)
-		//{
-		//	auto clone = layer->Clone();
-		//	clones[clone->Name()] = clone;
-		//}
+		// then connect them in the same manner as in original network and clone order
+		for (auto layer : sourceFlow.m_Order)
+		{
+			auto layerClone = clones[layer->Name()];
+			for (auto inLayer : layer->InputLayers())
+                layerClone->Link(clones[inLayer->Name()]);
 
-		//// then connect them in the same manner as in original network and clone order
-		//for (auto layer : sourceFlow.m_Order)
-		//{
-		//	auto layerClone = clones[layer->Name()];
-		//	for (auto inLayer : layer->InputLayers())
-		//	{
-		//		auto inLayerClone = clones[inLayer->Name()];
-		//		layerClone->InputLayers().push_back(inLayerClone);
-		//		inLayerClone->OutputLayers().push_back(layerClone);
-		//	}
+			m_Order.push_back(layerClone);
+		}
 
-		//	m_Order.push_back(layerClone);
-		//}
+		m_ReversedOrder.resize(m_Order.size());
+		reverse_copy(m_Order.begin(), m_Order.end(), m_ReversedOrder.begin());
 
-		//m_ReversedOrder.resize(m_Order.size());
-		//reverse_copy(m_Order.begin(), m_Order.end(), m_ReversedOrder.begin());
+        for (auto layer : sourceFlow.m_ModelInputLayers)
+            m_ModelInputLayers.push_back(clones[layer->Name()]);
 
-  //      for (auto layer : sourceFlow.m_ModelInputLayers)
-		//{
-		//	auto layerClone = clones[layer->Name()];
-  //          m_ModelInputLayers.push_back(layerClone);
-		//}
-
-  //      for (auto layer : sourceFlow.m_ModelOutputLayers)
-		//{
-		//	auto layerClone = clones[layer->Name()];
-  //          m_ModelOutputLayers.push_back(layerClone);
-		//}
+        for (auto layer : sourceFlow.m_ModelOutputLayers)
+            m_ModelOutputLayers.push_back(clones[layer->Name()]);
 	}
 
     //////////////////////////////////////////////////////////////////////////
