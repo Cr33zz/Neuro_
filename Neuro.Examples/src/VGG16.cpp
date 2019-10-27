e4f7e927ff780858c976fd747cc770424ec0e73e
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-10-27 17:01:30 -0400

    Added name scope for vgg16 preprocess operation.

--- Neuro.Examples/src/VGG16.cpp
+++ Neuro.Examples/src/VGG16.cpp
@@ -59,5 +59,6 @@ Neuro::ModelBase* VGG16::CreateModel(EDataFormat dataFormat, Shape inputShape, b
 //////////////////////////////////////////////////////////////////////////
 TensorLike* VGG16::Preprocess(TensorLike* image, EDataFormat dataFormat)
 {
+    NameScope scope("vgg_preprocess");
     return sub(image, new Constant(Tensor({ 103.939f, 116.779f, 123.68f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3))));
 }
