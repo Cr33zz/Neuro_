6ff1ff7e42ea69938dd7256416ee97991ca27a69
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-05 13:21:15 -0500

    Added variance operation.

---
+++ Neuro/include/ComputationalGraph/Operations/VarianceOp.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ComputationalGraph/NameScope.h"
+#include "ComputationalGraph/Operation.h"
+#include "ComputationalGraph/Operations/MeanOp.h"
+#include "ComputationalGraph/Operations/PowOp.h"
+#include "ComputationalGraph/Operations/SubtractOp.h"
+#include "ComputationalGraph/Operations/SqrtOp.h"
+
+namespace Neuro
+{
+    static Operation* variance(TensorLike* input, TensorLike* inputMean, float epsilon, EAxis axis = GlobalAxis, const string& name = "")
+    {
+        NameScope scope("variance");
+        if (!inputMean)
+            inputMean = mean(input, axis);
+        auto xMu = sub(input, inputMean);
+        return mean(square(xMu), axis);
+    }
+}
