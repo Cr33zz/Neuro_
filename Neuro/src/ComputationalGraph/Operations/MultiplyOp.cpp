a5c7820341ee71d5e1b7b200caae3198edd4c339
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-05 23:07:05 -0500

    Fixed crash when multiplying by zero in multiply op.

--- Neuro/src/ComputationalGraph/Operations/MultiplyOp.cpp
+++ Neuro/src/ComputationalGraph/Operations/MultiplyOp.cpp
@@ -26,7 +26,7 @@ namespace Neuro
     //////////////////////////////////////////////////////////////////////////
     void MultiplyOp::ComputeInternal()
     {
-        if (m_Val)
+        if (m_Inputs.size() == 1)
         {
             m_Output.ResizeBatch(m_Inputs[0]->Batch());
             m_Inputs[0]->Mul(m_Val, m_Output);
