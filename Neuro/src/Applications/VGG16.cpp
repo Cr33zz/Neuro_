d8ca7d782e6e841bb90d16032d24a80dc55b2808
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-16 15:26:44 -0500

    Tiny refactor of image processing.

--- Neuro/src/Applications/VGG16.cpp
+++ Neuro/src/Applications/VGG16.cpp
@@ -14,7 +14,8 @@ namespace Neuro
     //////////////////////////////////////////////////////////////////////////
     void VGG16::PreprocessImage(Tensor& image, EDataFormat dataFormat, bool swapChannels)
     {
-        if (swapChannels)
+        image.Sub(Tensor({ 123.68f, 116.779f, 103.939f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3)), image);
+        if (swapChannels) // weights in first conv layer of VGG are expecting image in BGR format
         {
             Tensor temp(image);
             //convert RGB to BGR
@@ -23,18 +24,15 @@ namespace Neuro
                 temp.CopyDepthTo(0, n, 2, n, image);
                 temp.CopyDepthTo(2, n, 0, n, image);
             }
-            image.Sub(Tensor({ 103.939f, 116.779f, 123.68f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3)), image);
         }
-        else
-            image.Sub(Tensor({ 123.68f, 116.779f, 103.939f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3)), image);
     }
 
     //////////////////////////////////////////////////////////////////////////
     void VGG16::DeprocessImage(Tensor& image, EDataFormat dataFormat, bool swapChannels, bool clipValues)
     {
+        image.Add(Tensor({ 123.68f, 116.779f, 103.939f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3)), image);
         if (swapChannels)
         {
-            image.Add(Tensor({ 103.939f, 116.779f, 123.68f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3)), image);
             Tensor temp(image);
             //convert BGR to RGB
             for (uint32_t n = 0; n < image.Batch(); ++n)
@@ -43,8 +41,6 @@ namespace Neuro
                 temp.CopyDepthTo(2, n, 0, n, image);
             }
         }
-        else
-            image.Add(Tensor({ 123.68f, 116.779f, 103.939f }, dataFormat == NHWC ? Shape(3) : Shape(1, 1, 3)), image);
 
         if (clipValues)
             image.Clipped(0, 255, image);
