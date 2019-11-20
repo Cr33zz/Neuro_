7938afcc320248b59899ac4fc10844dce1b8282f
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-20 12:43:04 -0500

    Changing total variation.

---
+++ Neuro/include/ComputationalGraph/Operations/TotalVariationOp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ComputationalGraph/Constant.h"
+#include "ComputationalGraph/Operations/Conv2DOp.h"
+
+namespace Neuro
+{
+    static Operation* total_variation(TensorLike* x, const string& name = "")
+    {
+        static Tensor horizKernel({ 1, -1, 0, 0, 0, 0,
+                                    0, 0, 1, -1, 0, 0,
+                                    0, 0, 0, 0, 1, -1 }, Shape(2, 1, 3, 3), "horiz_kernel");
+        static Tensor vertKernel({ 1, -1, 0, 0, 0, 0,
+                                   0, 0, 1, -1, 0, 0,
+                                   0, 0, 0, 0, 1, -1 }, Shape(1, 2, 3, 3), "vert_kernel");
+
+        auto horizDiff = conv2d(x, new Constant(horizKernel), 1, 0, NCHW, "horiz_diff");
+        auto vertDiff = conv2d(x, new Constant(vertKernel), 1, 0, NCHW, "vert_diff");
+
+        //return add(sum(abs(horizDiff), _012Axes), sum(abs(vertDiff), _012Axes));
+        return nullptr;
+    }
+}
---
+++ Neuro/src/ComputationalGraph/Operations/TotalVariationOp.cpp
@@ -0,0 +1,6 @@
+#include "ComputationalGraph/Operations/TotalVariationOp.h"
+
+namespace Neuro
+{
+
+}
