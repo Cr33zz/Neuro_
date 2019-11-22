49c1fca959b3b920254410b508029d4d4cd348e1
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-21 19:50:49 -0500

    .

--- Neuro.Examples/src/main.cpp
+++ Neuro.Examples/src/main.cpp
@@ -28,65 +28,38 @@ int main()
     //Tensor input(Shape(512, 512, DEPTH_IN, 6)); input.FillWithRand();
     //Tensor kernels(Shape(3, 3, DEPTH_IN, KERNELS_NUM)); kernels.FillWithRand();
     //Tensor output(Tensor::GetConvOutputShape(input.GetShape(), KERNELS_NUM, 3, 3, 1, 0, 0, NCHW));
-
-    //const int ITERS = 10;
-
-    //for (int i = 0; i < ITERS; ++i)
-    //{
-    //    AutoStopwatch prof(Microseconds);
-    //    input.Conv2D(kernels, 1, 0, NCHW, output);
-    //    cout << i << ": " << prof.ToString() << endl;
-    //}
-
+    ////Tensor output(Tensor::GetPooling2DOutputShape(input.GetShape(), 3, 3, 1, 0, 0, NCHW));
     //input.CopyToDevice();
     //kernels.CopyToDevice();
     //output.OverrideDevice();
-    //
-    //cudnnConvolutionDescriptor_t convolutionDesc; cudnnCreateConvolutionDescriptor(&convolutionDesc);
-    //cudnnTensorDescriptor_t inputDesc; cudnnCreateTensorDescriptor(&inputDesc);
-    //cudnnFilterDescriptor_t kernelsDesc; cudnnCreateFilterDescriptor(&kernelsDesc);
-    //cudnnTensorDescriptor_t outputDesc; cudnnCreateTensorDescriptor(&outputDesc);
 
-    //cudnnSetConvolution2dDescriptor(convolutionDesc, 0, 0, 1, 1, 1, 1, CUDNN_CROSS_CORRELATION, CUDNN_DATA_FLOAT);
-    //cudnnSetFilter4dDescriptor(kernelsDesc, CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, kernels.GetShape().Dimensions[3], kernels.GetShape().Dimensions[2], kernels.GetShape().Dimensions[1], kernels.GetShape().Dimensions[0]);
-    //cudnnSetTensor4dDescriptor(inputDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, input.GetShape().Dimensions[3], input.GetShape().Dimensions[2], input.GetShape().Dimensions[1], input.GetShape().Dimensions[0]);
-    //cudnnSetTensor4dDescriptor(outputDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, output.GetShape().Dimensions[3], output.GetShape().Dimensions[2], output.GetShape().Dimensions[1], output.GetShape().Dimensions[0]);
+    //Tensor x(output.GetShape()); x.FillWithRand();
+    //Tensor y(output.GetShape()); y.FillWithRand(-1, 1, 5);
 
-    //cudnnConvolutionFwdAlgo_t algo = CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM;
-    ////CUDA_CHECK(cudnnGetConvolutionForwardAlgorithm(s_CudnnHandle, inputDesc, kernelsDesc, convolutionDesc, outputDesc, CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 0, &algo));
+    //x.CopyToDevice();
+    //y.CopyToDevice();
+    //output.OverrideDevice();
 
-    //size_t workspaceSize;
-    //CUDA_CHECK(cudnnGetConvolutionForwardWorkspaceSize(TensorOpGpu::s_CudnnHandle, inputDesc, kernelsDesc, convolutionDesc, outputDesc, algo, &workspaceSize));
-    //void* workspacePtr;
-    //DeviceMemoryManager::Default().Allocate(&workspacePtr, workspaceSize, "conv2d_workspace");
+    //void* workspacePtr = nullptr;
 
-    //for (int i = 0; i < ITERS; ++i)
+    //for (int i = 0; i < 40; ++i)
     //{
-    //    AutoStopwatch prof(Microseconds);
+    //    AutoStopwatch p1(Microseconds);
+    //    x.Div(y, output);
+    //    cout << "Div " << p1.ToString() << endl;
 
-    //    output.Zero();
+    //    AutoStopwatch p2(Microseconds);
+    //    input.Conv2D(kernels, 1, 0, NCHW, output);
+    //    cout << "Other " << p2.ToString() << endl;
 
-    //    float alpha = 1, beta = 0;
-    //    CUDA_CHECK(cudnnConvolutionForward(
-    //        TensorOpGpu::s_CudnnHandle,
-    //        &alpha,
-    //        inputDesc,
-    //        input.GetDevicePtr(),
-    //        kernelsDesc,
-    //        kernels.GetDevicePtr(),
-    //        convolutionDesc,
-    //        algo,
-    //        workspacePtr,
-    //        workspaceSize,
-    //        &beta,
-    //        outputDesc,
-    //        output.GetDevicePtr()));
-
-    //    cout << i << ": " << prof.ToString() << endl;
+    //    AutoStopwatch p3(Microseconds);
+    //    output.SyncToHost();
+    //    cout << "Sync " << p3.ToString() << endl;
     //}
 
-    //DeviceMemoryManager::Default().Free(workspacePtr);
+    ////cin.get();
 
+    //return 0;
 
     //ComputationalGraph().Run();
     //IrisNetwork().Run();
@@ -105,6 +78,5 @@ int main()
     AdaptiveStyleTransfer().Run();
     //AdaptiveStyleTransfer().Test();
 
-    cin.get();
     return 0;
 }
