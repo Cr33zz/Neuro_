32810c7aa1267e460e49eaf6d748c5c1284031f6
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-22 18:12:14 -0500

    More precise data preloader profile tags.

--- Neuro/src/DataPreloader.cpp
+++ Neuro/src/DataPreloader.cpp
@@ -23,9 +23,9 @@ namespace Neuro
     //////////////////////////////////////////////////////////////////////////
     void DataPreloader::Load()
     {
-        NVTXProfile p("Loading preloaded data", 0xFF93FF72);
         vector<Tensor>* data = nullptr;
         {
+            NVTXProfile p("Waiting for available data", 0xFF93FF72);
             unique_lock<mutex> availableLocker(m_AvailableMtx);
             m_AvailableCond.wait(availableLocker, [this]() {return !m_Available.empty(); });
 
@@ -33,11 +33,15 @@ namespace Neuro
             m_Available.pop_front();
         }
 
-        // copy data to placeholders
-        for (size_t i = 0; i < m_Destination.size(); ++i)
-            (*data)[i].CopyTo(m_Destination[i]->Output());
+        {
+            NVTXProfile p("Copying preloaded data to placeholders", 0xFF93FF72);
+            // copy data to placeholders
+            for (size_t i = 0; i < m_Destination.size(); ++i)
+                (*data)[i].CopyTo(m_Destination[i]->Output());
+        }
 
         {
+            NVTXProfile p("Waiting for pending data lock", 0xFF93FF72);
             unique_lock<mutex> pendingLocker(m_PendingMtx);
             m_Pending.push_back(data);
         }
@@ -55,6 +59,7 @@ namespace Neuro
             vector<Tensor>* data = nullptr;
 
             {
+                NVTXProfile p("Waiting for pending data", 0xFF93FF72);
                 unique_lock<mutex> pendingLocker(m_PendingMtx);
                 m_PendingCond.wait(pendingLocker, [this]() {return !m_Pending.empty() || m_Stop; });
 
@@ -65,11 +70,15 @@ namespace Neuro
                 m_Pending.pop_front();
             }
 
-            // load data
-            for (size_t i = 0; i < m_Loaders.size(); ++i)
-                (*m_Loaders[i])((*data)[i]);
+            {
+                NVTXProfile p("Loading data", 0xFF93FF72);
+                // load data
+                for (size_t i = 0; i < m_Loaders.size(); ++i)
+                    (*m_Loaders[i])((*data)[i]);
+            }
 
             {
+                NVTXProfile p("Waiting for available data lock", 0xFF93FF72);
                 unique_lock<mutex> availableLocker(m_AvailableMtx);
                 m_Available.push_back(data);
             }
