30d215b81bec52e8ffb34081d4ff147f8f7a8708
Cr33zz <pawel.creez.kubiak@gmail.com>
2020-01-19 15:45:29 -0500

    Added a param to StyleLossFromGram.

--- Neuro.Examples/src/NeuralStyleTransfer.cpp
+++ Neuro.Examples/src/NeuralStyleTransfer.cpp
@@ -10,25 +10,27 @@ TensorLike* GramMatrix(TensorLike* features, const string& name)
     auto reshapedFeatures = reshape(features, Shape(featureMapSize, features->GetShape().Depth()));
     return div(matmul(reshapedFeatures, transpose(reshapedFeatures)), (float)reshapedFeatures->GetShape().Length, "result");
     //return div(matmul(features, transpose(features)), (float)featureMapSize, "result");
+    //return matmul(reshapedFeatures, transpose(reshapedFeatures));
 }
 
 //////////////////////////////////////////////////////////////////////////
 TensorLike* StyleLoss(TensorLike* styleGram, TensorLike* stylizedFeatures, int index)
 {
     assert(stylizedFeatures->GetShape().Batch() == 1);
-    return StyleLossFromGram(styleGram, GramMatrix(stylizedFeatures, "stylized" + to_string(index)), index);
+    return StyleLossFromGram(styleGram, GramMatrix(stylizedFeatures, "stylized" + to_string(index)), stylizedFeatures->GetShape(), index);
 }
 
 //////////////////////////////////////////////////////////////////////////
-TensorLike* StyleLossFromGram(TensorLike* styleGram, TensorLike* stylizedGram, int index)
+TensorLike* StyleLossFromGram(TensorLike* styleGram, TensorLike* stylizedGram, const Shape& featuresShape, int index)
 {
     NameScope scope("style_loss_" + to_string(index));
     
-    /*auto& styleFeaturesShape = stylizedFeatures->GetShape();
-    float height = (float)styleFeaturesShape.Height(), width = (float)styleFeaturesShape.Width(), channels = (float)styleFeaturesShape.Depth();*/
     return sum(square(sub(styleGram, stylizedGram)));
-    //return mean(square(sub(targetStyleGram, styleGram)));
+    //return mean(square(sub(styleGram, stylizedGram)));
+
+    //float height = (float)featuresShape.Height(), width = (float)featuresShape.Width(), channels = (float)featuresShape.Depth();
     //return div(sum(square(sub(targetStyleGram, styleGram))), 4.f * ::pow(channels, 2) * ::pow(width * height, 2));
+    //return div(sum(square(sub(styleGram, stylizedGram))), channels * width * height);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -36,7 +38,7 @@ TensorLike* ContentLoss(TensorLike* contentFeatures, TensorLike* stylizedFeature
 {
     NameScope scope("content_loss");
     auto& shape = stylizedFeatures->GetShape();
-    //return mean(square(sub(targetContentFeatures, contentFeatures)));
-    //return l2_loss(sub(targetContentFeatures, contentFeatures));
+    //return mean(square(sub(contentFeatures, stylizedFeatures)));
+    //return l2_loss(sub(contentFeatures, stylizedFeatures));
     return div(sum(square(sub(contentFeatures, stylizedFeatures))), (float)shape.Width() * shape.Height() * shape.Depth());
 }
