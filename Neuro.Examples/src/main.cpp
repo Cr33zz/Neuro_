a6f9077663094faacf90779e521c20f35369726c
Cr33zz <pawel.creez.kubiak@gmail.com>
2020-02-24 08:05:02 -0500

    Added private extension library include. Cleanup.

--- Neuro.Examples/src/main.cpp
+++ Neuro.Examples/src/main.cpp
@@ -13,51 +13,10 @@
 #include "FastNeuralStyleTransfer.h"
 #include "AdaptiveStyleTransfer.h"
 #include "Pix2Pix.h"
-
-#include <cuda.h>
-#include <cudnn.h>
-#include "Tensors/TensorOpGpu.h"
-#include "Tensors/Cuda/CudaErrorCheck.h"
+//#include "NeuralStyleTransferHD2.h"
 
 int main()
 {
-    //Tensor::SetForcedOpMode(GPU);
-    //cin.get();
-
-    //{
-    //    auto trainFiles = LoadFilesList("f:/!TrainingData/flowers", false, true);
-
-    //    const Shape IMG_SHAPE(256, 256, 3);
-    //    const uint32_t PATCH_SIZE = 64;
-    //    const uint32_t BATCH_SIZE = 4;
-
-    //    Tensor condImages(Shape::From(IMG_SHAPE, BATCH_SIZE), "cond_image");
-    //    Tensor realImages(Shape::From(IMG_SHAPE, BATCH_SIZE), "output_image");
-
-    //    Pix2Pix::Pix2PixImageLoader loader(trainFiles, BATCH_SIZE, 1, 1337);
-    //    DataPreloader preloader({ &condImages, &realImages }, { &loader }, 5, false);
-
-    //    //cin.get();
-    //    for (int i = 0; i < 100; ++i)
-    //    {
-    //        /*Tensor t(Shape(256,256,3,1), "t");
-    //        t.FillWithRand(-1, 0, 255);
-    //        Tensor e = CannyEdgeDetection(t);
-    //        e.Name("e");
-    //        cout << e.DataPtrUnsafe()[0] << endl;*/
-    //        preloader.Load();
-
-    //        /*if (i == 2)
-    //        {
-    //            DumpMemoryManagers("mem_1.log");
-    //            cin.get();
-    //        }*/
-    //    }
-    //}
-    //ReleaseAllMemory();
-    //DumpMemoryManagers("mem_2.log");
-    //cin.get();
-
     //ComputationalGraph().Run();
     //IrisNetwork().Run();
     //ConvNetwork().Run();
@@ -72,10 +31,11 @@ int main()
     //CifarGAN().Run();
     //CifarGAN().RunDiscriminatorTrainTest();
     //NeuralStyleTransfer().Run();
+    //NeuralStyleTransferHD2().Run();
     //FastNeuralStyleTransfer().Run();
     //AdaptiveStyleTransfer().Run();
     //AdaptiveStyleTransfer().Test();
-    Pix2Pix().Run();
+    //Pix2Pix().Run();
     //Pix2Pix().RunDiscriminatorTrainTest();
 
     return 0;
